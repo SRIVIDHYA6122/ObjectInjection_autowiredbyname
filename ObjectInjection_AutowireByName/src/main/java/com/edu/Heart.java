f55f111bce891d3786fa0dac6596cfa16df55a0b
SRI VIDHYA C <SRI VIDHYA C@LAPTOP-G9VMJ633>
2022-04-29 12:47:32 +0530

    autowired by namae by using object

---
+++ ObjectInjection_AutowireByName/src/main/java/com/edu/Heart.java
@@ -0,0 +1,12 @@
+package com.edu;
+
+public class Heart {
+
+	public void pump() {
+		System.out.println("Hearting is pumping");
+		System.out.println("Alive");
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ ObjectInjection_AutowireByName/src/main/java/com/edu/Human.java
@@ -0,0 +1,24 @@
+package com.edu;
+
+public class Human {
+	private Heart heart;
+
+	public Heart getHeart() {
+		return heart;
+	}
+
+	public void setHeart(Heart heart) {
+		this.heart = heart;
+	}
+	public void humanFunction() {
+		if(heart!=null)
+		{
+			heart.pump();
+		}
+		else
+		{
+			System.out.println("Not Alive");
+		}
+	}
+
+}
